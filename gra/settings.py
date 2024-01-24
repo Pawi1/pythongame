0445ffb57f7a448e6ffb7efd19e90571ecf1a54a
Pawi1 <p.rutkow@o2.pl>
2024-01-24 12:40:08 +0100

    Fix css file x11

--- gra/settings.py
+++ gra/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = BASE_DIR / 'static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -120,8 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    BASE_DIR / 'gra/static',
+    # BASE_DIR / 'gra/static',
 ]
 
 # Default primary key field type
