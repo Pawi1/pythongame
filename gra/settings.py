831e8080089d985bc16656282ac123cf0f75f6f6
Pawi1 <p.rutkow@o2.pl>
2024-01-24 11:21:49 +0100

    Fix css file x7

--- gra/settings.py
+++ gra/settings.py
@@ -47,8 +47,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'gra.urls'
