f41671c1da845aff1152113a4b58db065f9cf0f6
Pawi1 <p.rutkow@o2.pl>
2024-01-24 13:17:52 +0100

    Fix css file x15

--- gra/urls.py
+++ gra/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
 from .views import execute_command
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path
 
 urlpatterns = [
-    path('', execute_command, name='execute_command')
+    path('', execute_command, name='execute_command'),
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
