805ad059ab4b93f02ac511e7f9d6f263eb957ff6
pawi1 <p.rutkow@o2.pl>
2024-12-20 21:15:19 +0100

    Naprawa settings.py

--- gra/settings.py
+++ gra/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ""
-with open("../my_secret_key.txt", "r") as file:
+with open("my_secret_key.txt", "r") as file:
     SECRET_KEY = file.readline().rstrip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
